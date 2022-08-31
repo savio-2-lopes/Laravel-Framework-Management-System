4e2a80cf5b5fc1346190236892052b24b91abec2
savio-2-lopes <savioaugulopes@gmail.com>
2022-08-30 23:00:46 -0300

    Change SoftDelete migration

--- app/app/SiteContato.php
+++ app/app/SiteContato.php
@@ -4,7 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+// Site_contato_Teste
+// Site_contato_teste
+// Site_contato_testes
+
 class SiteContato extends Model
 {
     //
+    protected $table = "fornecedores";
 }
